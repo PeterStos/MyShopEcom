60318370d725312210552070220344245bdb37c7
peterson <petersantos000@gmail.com>
2020-04-01 13:27:08 -0400

    Added Product Upload Images

--- MyEcomShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyEcomShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -3,7 +3,9 @@ using MyShop.Core.Models;
 using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MyShop.WebUI.Controllers
@@ -50,7 +52,7 @@ namespace MyShop.WebUI.Controllers
 
         // Display a page to fill the details of the products
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -58,6 +60,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
+                if (file != null) // test if file exists
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName); // give name to the file
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image); // save the file in the folder
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -85,7 +92,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -98,10 +105,15 @@ namespace MyShop.WebUI.Controllers
                 {
                     return View(product);
                 }
+                
+                if (file != null) // test if file exists
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName); // give name to the file
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image); // save the file in the folder
+                }
 
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
 
