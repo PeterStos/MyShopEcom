b94c8cee2c61d348b9e1c78c65ba1d1827bd7883
peterson <petersantos000@gmail.com>
2020-04-07 01:29:44 -0400

    Added Basket Model

---
+++ MyEcomShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        /// <summary>
+        /// Constructor to create an empty list of basket items on creation
+        /// </summary>
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
